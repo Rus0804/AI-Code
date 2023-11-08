172355fb690127e8742617e870bdf59611f5bda8
Rus0804 <125786226+Rus0804@users.noreply.github.com>
2023-11-08 19:54:39 +0530

    Add files via upload

---
+++ AI.CIA.py
@@ -0,0 +1,44 @@
+# SNU_CHENNAI.
+
+# L=[]
+# n=int(input("Enter number of substrings: "))
+# for i in range(n):
+#     substring=input("Prompt",i,":")
+#     L.append(substring)
+
+l=['CHEN','U_CH','SNU_']
+
+def check(s, l, i, c):
+    if l[i] in s and l[i] != l[-1]:
+        return check(s, l, i + 1, c)
+    else:
+        if s[-2:] == l[i][:-2]:
+            s += l[i][-2:]
+        elif l[i][-2:] == s[:2]:
+            s = l[i] + s[2:]
+        elif s == '':
+            s = l[i]
+
+        if i == len(l) - 1:
+            l2=[s]
+            t = True
+            if c!=len(l):
+                for j in l:
+                    if j not in s:
+                        t = False
+            else:
+                for j in l:
+                    if j not in s:
+                        l2.append(j)
+                return [s]+check('',l2[1:],0,0)
+            if t:
+                return l2
+            else:
+                c+=1
+                return check(s, l, 0, c)
+        else:
+            return check(s, l, i + 1, c)
+
+RESULT = check('', l, 0, 0)
+for i in RESULT:
+    print("Seed",RESULT.index(i)+1,":",i)
